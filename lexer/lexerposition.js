c7a2233f3cb681d3e5e4c8fb9453854587475314
olex-bel <>
2021-11-25 21:28:32 +0100

    Fixed column position.

--- lexer/lexerposition.js
+++ lexer/lexerposition.js
@@ -1,11 +1,11 @@
 function LexerPosition() {
     this.line = 1;
-    this.column = 0;
+    this.column = 1;
 }
 
 LexerPosition.prototype.increaseLineNumber = function () {
     this.line += 1;
-    this.column = 0;
+    this.column = 1;
 };
 LexerPosition.prototype.increaseColumnNumber = function () {
     this.column += 1;
